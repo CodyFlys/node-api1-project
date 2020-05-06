e21b8a33ddcca7eb8901887d5e0686f93113c3e9
Cody <codysolomon15@gmail.com>
2020-05-05 17:10:35 -0700

    completed project correctly

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ const server = express(); // creates the server using express
 server.use(express.json())
 
 let users = [
-    { id: "1", name: "Jane Doe", bio: "Not Tarzan's Wife, another Jane" },
-    { id: "2", name: "John Apples", bio: "Not apple, just John" },
-    { id: "3", name: "Apples", bio: "Not john, only apples" },
+    { id: 1, name: "Jane Doe", bio: "Not Tarzan's Wife, another Jane" },
+    { id: 2, name: "John Apples", bio: "Not apple, just John" },
+    { id: 3, name: "Apples", bio: "Not john, only apples" },
 ]
+
+allIds = users.map(user => {
+    return user.id
+})
+console.log(allIds)
 
 
 server.get('/', (req, res) => {
@@ -16,8 +21,6 @@ server.get('/', (req, res) => {
 })
 
 server.get('/api/users', function (req, res) {
-    // const users = db.getUsers()
-
     if (users) {
         res.json(users)
     } else {
@@ -27,63 +30,65 @@ server.get('/api/users', function (req, res) {
     }
 })
 
-server.post('/api/users', function (req, res) {
-    const newUser = req.body;
+server.post("/api/users", (req, res) => {
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name,
+        bio: req.body.bio
+    }
     users.push(newUser)
 
-    if (users.includes(newUser)) {
+    if (req.body.name.length == 0 || req.body.bio.length == 0) {
+        res.status(400).json({errorMessage: "Please provide name and bio for the user." })
+    } else if(users.includes(newUser)) {
         res.status(200).json(newUser)
     } else {
-        return res.status(500).json({
-            errorMessage: "The users information could not be retrieved."
-        })
+        res.status(500).json({errorMessage: "There was an error while saving the user to the database" })
     }
 })
 
-server.delete('/api/users/:id', function (req, res) {
+server.delete('/api/users/:id', (req, res) => {
     const id = req.params.id
 
-    users.forEach((user) => {
-        if(user.id == id){
-            users = users.filter(user => user.id != id)
-            res.status(200).json(users)
-        } else if (user.id != id) {
-            res.status(404).json({errorMessage: "DELETE User with the specified ID does not exist."})
-        } else {
-            res.status(500).json({errorMessage: "DELETE User cannot not be removed"})
-        }
-    })
+    if (!users) {
+        res.status(500).json({errorMessage: "The user could not be removed" })
+    } else if(allIds.includes(Number(id))) {
+        users = users.filter(user => user.id != id)
+        res.status(200).json(users)
+    } else {
+        res.status(404).json({message: "The user with the specified ID does not exist."})
+    }
 })
 
 server.put('/api/users/:id', function (req, res) {
     const id = req.params.id
-
-    users.forEach((user) => {
-        if(user.id == id){
-            user.name = req.body.name || user.name
-            user.bio = req.body.bio || user.bio,
-            res.status(200).json(users)
-        } else if (user.id != id) {
-            res.status(404).json({errorMessage: "User with the specified ID does not exist."})
-        } else {
-            res.status(500).json({errorMessage: "User cannot not be removed"})
-        }
-    })
-
+    let updatedUser = users.filter(user => user.id == id)
+    if (req.body.name.length == 0 || req.body.bio.length == 0) {
+        res.status(400).json({errorMessage: "Please provide name and bio for the user." })
+    } else if(allIds.includes(Number(id))) {
+        users[id - 1].name = req.body.name
+        users[id - 1].bio = req.body.bio
+        res.status(200).json(users)
+    } else {
+        res.status(404).json({message: "The user with the specified ID does not exist."})
+    }
 })
 
-server.get('/api/users/:id', function (req, res) {
+server.get("/api/users/:id", (req, res) => {
     const id = req.params.id
-
-    users.forEach((user) => {
-        if(user.id == id){
-            res.status(200).json(user)
-        } else if (user.id != id) {
-            res.status(404).json({message: "The user with the specified ID does not exist."})
-        } else {
-            res.status(500).json({errorMessage: "The user information could not be retrieved."})
-        }
+    foundUser = users.filter(user => user.id == id)
+    allIds = users.map(user => {
+        return user.id
     })
+    console.log(allIds)
+
+    if (allIds.includes(Number(id))) {
+        res.status(200).json(foundUser)
+    } else if (!users) {
+        res.status(500).json({errorMessage: "There was an error while saving the user to the database" })
+    } else {
+        res.status(404).json({message: "The user with the specified ID does not exist."})
+    }
 })
 
 server.listen(5000, () => 
