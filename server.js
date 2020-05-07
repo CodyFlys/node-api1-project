7b6f71c1952f6f81614251fefb651e69bba8b631
Cody <codysolomon15@gmail.com>
2020-05-07 01:48:50 -0700

    got it

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express'); // imports the express package
 const db = require("./database.js");
 const server = express(); // creates the server using express
-require=('dotenv').config();
+require('dotenv').config();
 
 const port = process.env.PORT;
 
